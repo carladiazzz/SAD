53979589b59403a6b734ece038c50c841ed28417
oriolcm02 <145464717+oriolcm02@users.noreply.github.com>
2023-09-26 14:52:08 +0200

    Add files via upload

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,30 +1,69 @@
-package P0;
+//package P0;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 
-public class EditableBufferedReader extends BufferedReader
+public class EditableBufferedReader extends BufferedReader{
 
 public EditableBufferedReader(Reader text){
     super(text);
 }
 
 public void setRaw(){
- String[] comanda = {"/bin/sh", "-c", "stty raw </dev/tty"}; // /bin/sh, per a executar el codi com a una cadena, i amb l'stty configurem el terminal en raw mode
- Runtime.getRuntime().exec(comanda).waitFor(); //Executem la comanda i esperem a que aquesta acabi
+ String[] comanda = {"/bin/sh", "-c", "stty raw -echo </dev/tty"}; // /bin/sh, per a executar el codi com a una cadena, i amb l'stty configurem el terminal en raw mode
+    try{
+        Runtime.getRuntime().exec(comanda).waitFor(); //Executem la comanda i esperem a que aquesta acabi
+    }
+    catch(IOException ex){
+        System.err.println("Excepció a setRaw()");
+            ex.printStackTrace();
+    }
+    catch(InterruptedException ex){
+        System.err.println("Excepció a setRaw()");
+            ex.printStackTrace();
+    }
 }
 
-public unsetRaw(){  //Fem el mateix procès que abans, pero ara per tornar al mode 'cooked'
- String[] comanda = {"/bin/sh", "-c", "stty cooked </dev/tty"}
- Runtime.getRuntime().exec(comanda).waitFor();
+public void unsetRaw(){  //Fem el mateix procès que abans, pero ara per tornar al mode 'cooked'
+ String[] comanda = {"/bin/sh", "-c", "stty cooked echo </dev/tty"};
+    try{
+        Runtime.getRuntime().exec(comanda).waitFor();
+    }
+    catch(IOException ex){
+        System.err.println("Excepció a unsetRaw()");
+        ex.printStackTrace();
+    }
+    catch(InterruptedException ex){
+        System.err.println("Excepció a setRaw()");
+            ex.printStackTrace();
+    }
 }
 
 public int read(){
-    int caracter= super.read();
+    int caracter=0;
+    setRaw();
+    try{
+    caracter= super.read();
+    }
+    catch(IOException ex){
+        ex.printStackTrace();
+    }
     return caracter;
 }
 
-public readLine(){
+public String readLine(){
+    //setRaw();
+    int car=0;
+    
+    car=read();
+    
+
+
+        unsetRaw();
+    
+return String.valueOf(car);
+    
+}
 
 }
