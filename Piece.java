dd2fcc07ca579b55a7c93c32f962f03be6ec3dde
oriolcm02 <145464717+oriolcm02@users.noreply.github.com>
2023-12-01 15:31:33 +0100

    /Chess/

---
+++ Piece.java
@@ -0,0 +1,12 @@
+
+public class Piece {
+    private String nombre;
+
+    public Piece(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
